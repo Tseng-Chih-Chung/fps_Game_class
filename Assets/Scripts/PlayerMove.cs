1bd167daf764f5c67a207cc0ded728c73b26261f
Tseng-Chih-Chung <chiao105031@gmail.com>
2024-05-02 12:00:35 +0800

    進度
    1.新增場景中的障礙物
    2.將玩家移動程式碼完善(更新到02-01-05)並以自己懂的方式註解
    3.將准心圖片放入名為Images的資料夾中，並將其Texture Type設成Sprite(2D and UI)
    4.新增材質球資料夾，並將材質球放入場景物件中(有使用GPU去渲染，讓CPU負擔相對少一些)

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,31 +5,54 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [Header("移動設定")]
-    public float moveSpeed;
-    public float jumpForce;
+    public float moveSpeed;     //走路速度
+    public float runSpeed;      //跑步速度
+    public float jumpForce;     //跳躍高度
+    public float jumpCooldown;  //跳躍間隔
+    public float groundDrag;
 
     [Header("按鍵綁定")]
-    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode jumpKey = KeyCode.Space;     //預設空白鍵為跳躍
+    public KeyCode RunKey = KeyCode.LeftShift;  //預設左Shift為跑步鍵
 
     [Header("基本設定")]
     public Transform PlayerCamera;   // 攝影機
 
-    private float horizontalInput;   // 左右方向按鍵的數值(-1 <= X <= +1)
-    private float verticalInput;     // 上下方向按鍵的數值(-1 <= Y <= +1)
+    [Header("地板確認")]
+    public float checkHight;           // 設定檢查是否在地面的高度
+    public LayerMask whatIsGround;     // 設定哪一個圖層是射線可以打到的
+    public bool isground;              // 布林變數：是否在地上
+
+    private bool isRun;             //是否正在跑步
+    private bool canJump;           // 設定是否可以跳躍
+    private float horizontalInput;  // 水平方向的力
+    private float verticalInput;    // 垂直方向的力
 
     private Vector3 moveDirection;   // 移動方向
 
-    private Rigidbody rbFirstPerson; // 第一人稱物件(膠囊體)的剛體
+    private Rigidbody rbFirstPerson; // 玩家的剛體
 
     private void Start()
     {
         rbFirstPerson = GetComponent<Rigidbody>();
+        rbFirstPerson.freezeRotation = true;         // 不讓該物件隨意旋轉(讓它永遠保持正確的姿勢)
+        canJump = true;
+        isRun = false;
     }
 
     private void Update()
     {
         MyInput();
         SpeedControl();   // 偵測速度，過快就減速
+
+        // 射出一條看不到的射線，來判斷有沒有打到地面？
+        isground = Physics.Raycast(transform.position, Vector3.down, checkHight * 0.5f + 0.3f, whatIsGround);
+        Debug.DrawRay(transform.position, new Vector3(0, -(checkHight * 0.5f + 0.3f), 0), Color.red); // 在測試階段將射線設定為紅色線條，來看看線條長度夠不夠？
+        // 如果碰到地板，就設定一個反作用力(這個可以製造人物移動的減速感)
+        if (isground)
+            rbFirstPerson.drag = groundDrag;
+        else
+            rbFirstPerson.drag = 0;
     }
 
     private void FixedUpdate()
@@ -43,19 +66,30 @@ public class PlayerMove : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        // 如果按下設定的跳躍按鍵
-        if (Input.GetKeyDown(jumpKey) == true)
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            Jump(); // 執行跳躍方法
+            isRun = true;
+        }
+        else
+        isRun = false;
+
+        // 如果按下設定的跳躍按鍵
+        if (Input.GetKeyDown(jumpKey) && canJump && isground)
+        {
+            canJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown); // 如果跳躍過後，就會依照設定的限制時間倒數，時間到了才能往上跳躍
         }
     }
 
     private void MovePlayer()
     {
         // 計算移動方向(其實就是計算X軸與Z軸兩個方向的力量)
-        moveDirection = PlayerCamera.forward * verticalInput + PlayerCamera.right * horizontalInput ;
-        // 推動第一人稱物件
-        rbFirstPerson.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        moveDirection = PlayerCamera.forward * verticalInput + PlayerCamera.right * horizontalInput;
+
+        // 如果在地面，則可以移動
+        if (isground)
+            rbFirstPerson.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
     }
 
     // 方法：偵測速度並減速
@@ -63,10 +97,19 @@ public class PlayerMove : MonoBehaviour
     {
         Vector3 flatVel = new Vector3(rbFirstPerson.velocity.x, 0f, rbFirstPerson.velocity.z); // 取得僅X軸與Z軸的平面速度
 
-        // 如果平面速度大於預設速度值，就將物件的速度限定於預設速度值
-        if (flatVel.magnitude > moveSpeed)
+        if(isRun!=true)
         {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            // 如果平面速度大於預設速度值，就將物件的速度限定於預設速度值(走路速度)
+            if (flatVel.magnitude > moveSpeed)
+            {
+               Vector3 limitedVel = flatVel.normalized * moveSpeed;
+               rbFirstPerson.velocity = new Vector3(limitedVel.x, rbFirstPerson.velocity.y, limitedVel.z);
+            }
+        }
+        else
+        {
+            //如果平面速度大於預設速度值，就將物件的速度限定於預設速度值(跑步速度)
+            Vector3 limitedVel = flatVel.normalized * runSpeed;
             rbFirstPerson.velocity = new Vector3(limitedVel.x, rbFirstPerson.velocity.y, limitedVel.z);
         }
     }
@@ -77,6 +120,12 @@ public class PlayerMove : MonoBehaviour
         // 重新設定Y軸速度
         rbFirstPerson.velocity = new Vector3(rbFirstPerson.velocity.x, 0f, rbFirstPerson.velocity.z);
         // 由下往上推第一人稱物件，ForceMode.Impulse可以讓推送的模式為一瞬間，會更像跳躍的感覺
-        rbFirstPerson.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        rbFirstPerson.AddForce(transform.up * jumpForce, ForceMode.Impulse);//Impulse大概的感覺是在一瞬間給完整個力
+    }
+
+    // 方法：重新設定變數readyToJump為true的方法
+    private void ResetJump()
+    {
+        canJump = true;
     }
 }
